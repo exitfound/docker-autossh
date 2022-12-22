c8ac0fab785ebdab5bd466ebd8c561185b06e534
medoed <medoed@kitezh.online>
2022-12-22 23:31:15 +0200

    Update run job

--- run-autossh-image.groovy
+++ run-autossh-image.groovy
@@ -10,16 +10,20 @@ pipeline {
     }
 
     parameters {
-        // Параметры для подключения к удаленному серверу:
+        // Параметры для подключения, если мы хотим организовать туннель на удаленном узле:
         string(name: 'SSH_USER', defaultValue: '', trim: true, description: 'Имя пользователя, которое будет использоваться для подключения к удаленному серверу:')
         string(name: 'SSH_HOST', defaultValue: '', trim: true, description: 'IP-адрес, который будет использоваться для подключения к удаленному серверу:')
-        string(name: 'SSH_PORT', defaultValue: '22', trim: true, description: 'Порт SSH для подключения к удаленному серверу, на котором будет выполняться команда:')
+
+        // Параметры для подключения к конечному серверу, откуда будет создаваться туннель:
+        string(name: 'SSH_TUNNEL_USER', defaultValue: '', trim: true, description: 'Имя пользователя, которое будет использоваться для подключения к конечному серверу при создании туннеля:')
+        string(name: 'SSH_TUNNEL_HOST', defaultValue: '', trim: true, description: 'IP-адрес, который будет использоваться для подключения к конечному серверу при создании туннеля:')
+        string(name: 'SSH_TUNNEL_PORT', defaultValue: '22', trim: true, description: 'Порт SSH для подключения к конечному серверу при создании туннеля:')
 
         // Параметры для настройки туннеля:
-        string(name: 'SSH_MODE', defaultValue: '-L', trim: true, description: 'Режим, в котором будет работать туннель (прямой или обратный). По умолчанию используется прямой:')        
-        string(name: 'SSH_TUNNEL_REMOTE_PORT', defaultValue: '', trim: true, description: 'Удаленный порт, который будет фигурировать на конечном инстансе, к которому будет подключаться контейнер через Autossh:')
+        string(name: 'SSH_TUNNEL_MODE', defaultValue: '-L', trim: true, description: 'Режим, в котором будет работать туннель (прямой или обратный). По умолчанию используется прямой:')        
+        string(name: 'SSH_TUNNEL_REMOTEPORT', defaultValue: '', trim: true, description: 'Удаленный порт, который будет фигурировать на конечном инстансе, к которому будет подключаться контейнер через Autossh:')
         string(name: 'SSH_TUNNEL_IP', defaultValue: '', trim: true, description: 'IP-адрес конечного инстанса, до которого будет прокинут туннель:')
-        string(name: 'SSH_TUNNEL_LOCAL_PORT', defaultValue: '', trim: true, description: 'Локальный порт, представляющий собой какой-либо сервис, который мы хотим прокинуть на наш конечный инстанс:')
+        string(name: 'SSH_TUNNEL_LOCALPORT', defaultValue: '', trim: true, description: 'Локальный порт, представляющий собой какой-либо сервис, который мы хотим прокинуть на наш конечный инстанс:')
     }
 
     stages {
@@ -28,7 +32,19 @@ pipeline {
                 withCredentials([sshUserPrivateKey(credentialsId: 'ssh_key_host', keyFileVariable: 'SSH_KEY_HOST')]){
                     sh '''
                     set +x
-                    ssh -i $SSH_KEY_HOST -o StrictHostKeyChecking=no ${SSH_USER}@${SSH_HOST} "sudo docker pull $DOCKERHUB_IMAGE:$DOCKERHUB_TAG"
+                    ssh -i $SSH_KEY_HOST -o StrictHostKeyChecking=no ${SSH_USER}@${SSH_HOST} "
+                    sudo docker pull $DOCKERHUB_IMAGE:$DOCKERHUB_TAG && \
+                    sudo docker run -d -e SSH_TUNNEL_PORT="${SSH_PORT}" \
+                    -e SSH_TUNNEL_MODE=${SSH_TUNNEL_MODE} \
+                    -e SSH_TUNNEL_REMOTEPORT=${SSH_TUNNEL_REMOTE_PORT} \
+                    -e SSH_TUNNEL_IP=${SSH_TUNNEL_IP} \
+                    -e SSH_TUNNEL_LOCALPORT=${SSH_TUNNEL_LOCAL_PORT}
+                    -e SSH_TUNNEL_USER="${SSH_USER}" \
+                    -e SSH_TUNNEL_HOST="${SSH_HOST}" \
+                    --name autossh \
+                    --network host \
+                    --restart unless-stopped \
+                    $DOCKERHUB_IMAGE:$DOCKERHUB_TAG"
                     '''
                 }
             }
