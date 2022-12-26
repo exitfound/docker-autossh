6f2c6541058a52660b83bf12cdf251f9314839f0
medoed <medoed@kitezh.online>
2022-12-27 00:20:01 +0200

    Change dockerfile name

--- autossh-run.groovy
+++ autossh-run.groovy
@@ -32,7 +32,7 @@ pipeline {
                 withCredentials([sshUserPrivateKey(credentialsId: 'ssh_key_image', keyFileVariable: 'SSH_KEY_IMAGE')]) {
                     sh('''
                     set +x
-                    sudo docker build -t "$DOCKERHUB_IMAGE:$DOCKERHUB_TAG" -f autossh-with-envs.dockerfile --build-arg SSH_PRV_KEY="$(cat $SSH_KEY_IMAGE)" .
+                    sudo docker build -t "$DOCKERHUB_IMAGE:$DOCKERHUB_TAG" -f autossh.dockerfile --build-arg SSH_PRV_KEY="$(cat $SSH_KEY_IMAGE)" .
                     ''')
                 }
             }
